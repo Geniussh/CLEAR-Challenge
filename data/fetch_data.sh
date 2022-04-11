ab3cb6a8faae7517b4188a7fb08e70a999ab17ae
Kelvin Shen <44150278+Geniussh@users.noreply.github.com>
2022-04-11 01:31:02 -0400

    Update and rename get_data.sh to fetch_data.sh

---
+++ data/fetch_data.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Setup
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+mkdir -p $DIR/cl_ext_mem
+mkdir -p $DIR/submissions
+
+echo "Downloading CLEAR10 dataset (train/validation set)..."
+
+echo "Unzipping data..."
+
+mv 
